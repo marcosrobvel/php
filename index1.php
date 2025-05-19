dd0d534dc411a5cdda54df777ddc08947afc3725
marcosrobvel <marcosraranjuez@gmail.com>
2025-05-19 11:45:16 +0200

    exercise-index1-php

---
+++ index1.php
@@ -0,0 +1,28 @@
+<?php
+$rooms = [
+    [
+        "ID" => 1,
+        "Name" => "Habitación Deluxe",
+        "Number" => 101,
+        "Price" => 120.50,
+        "Discount" => 10
+    ],
+    [
+        "ID" => 2,
+        "Name" => "Suite Familiar",
+        "Number" => 202,
+        "Price" => 180.00,
+        "Discount" => 15
+    ],
+    [
+        "ID" => 3,
+        "Name" => "Habitación Económica",
+        "Number" => 303,
+        "Price" => 80.99,
+        "Discount" => 5
+    ]
+];
+
+echo "<pre>";
+print_r($rooms);
+echo "</pre>";
